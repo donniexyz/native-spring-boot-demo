0d9d36d0910f25bb621d76eec70c5347ca8020ff
donnie <6776930+donniexyz@users.noreply.github.com>
2024-05-30 15:45:14 +0800

    Init project from spring initalizer

---
+++ src/test/java/com/github/donniexyz/demo/sbnative/NativeSpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.donniexyz.demo.sbnative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NativeSpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
